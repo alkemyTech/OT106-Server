4233c9fa4f424a6a8f3bf601d078ba202b10b551
root <j.gaston.altamirano@gmail.com>
2021-12-02 11:43:14 -0300

    jwt logic is added

---
+++ functions/get-token-from.js
@@ -0,0 +1,8 @@
+const getTokenFrom = (req) => {
+  const authorization = req.header('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+module.exports = getTokenFrom;
