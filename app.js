19752c2c1559213fc10fd320035babaa420fafd5
Aguirre Gonzalo Adolfo <gonza.a.aguirre@gmail.com>
2021-12-03 01:22:08 -0300

    feat(endpoint): adds route and changes controller to handle POST /auth/register endpoint

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/activities', activitiesRouter)
+app.use('/auth', require('./routes/auth-route'));
 
 
 app.use('/categories', categoriesRouter)
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,31 +1,44 @@
+const bcrypt = require('bcryptjs');
 const { UserService } = require('../services');
 const catchAsync = require('../functions/catchAsync');
 const { OK: OK_CODE, CREATED: CREATED_CODE } = require('../constants/httpStatus');
+const { OK: OK_MESSAGE } = require('../constants/message');
+
+const removePassword = (x) => {
+  return { ...x.dataValues, password: undefined };
+};
 
 module.exports = {
   findAllUsers: catchAsync(async (req, res, next) => {
     const result = await UserService.findAllUsers();
-    return res.status(OK_CODE).send(result);
+
+    // Removes the passwords of every result
+    const resultWithoutPasswords = result.map(x => removePassword(x));
+
+    return res.status(OK_CODE).send(resultWithoutPasswords);
   }),
 
   findUserByPk: catchAsync(async (req, res, next) => {
     const idToFind = req.params.id;
     const result = await UserService.findUserByPk(idToFind);
 
-    return res.status(OK_CODE).send(result);
+    return res.status(OK_CODE).send(removePassword(result));
   }),
 
   createUser: catchAsync(async (req, res, next) => {
+    // Hashs password
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     const attributes = {
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       email: req.body.email,
-      password: req.body.password,
-      photo: req.body.photo,
+      password: hashedPassword,
     };
     const result = await UserService.createUser(attributes);
 
-    return res.status(CREATED_CODE).send(result);
+    return res.status(CREATED_CODE).send(removePassword(result));
   }),
 
   updateUser: catchAsync(async (req, res, next) => {
@@ -37,15 +50,18 @@ module.exports = {
       password: req.body.password,
       photo: req.body.photo,
     };
-    const result = await UserService.updateUser(idToUpdate, attributes);
+    await UserService.updateUser(idToUpdate, attributes);
 
-    return res.status(OK_CODE).send(result);
+    // Finds the user updated and sent it as the result
+    const result = await UserService.findUserByPk(idToUpdate);
+
+    return res.status(OK_CODE).send(removePassword(result));
   }),
 
   destroyUser: catchAsync(async (req, res, next) => {
     const idToDelete = req.params.id;
-    const result = await UserService.destroyUser(idToDelete);
+    await UserService.destroyUser(idToDelete);
 
-    return res.status(OK_CODE).send(result);
+    return res.status(OK_CODE).send(OK_MESSAGE);
   }),
 };
