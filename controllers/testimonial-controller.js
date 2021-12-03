64b292ff4b4ec321e1d02877b57161db69937ca9
BuildTools <santiago.moreno.2009@gmail.com>
2021-12-02 23:18:43 -0300

    refactor: fixed http response messages/codes imports, replaced `res.send` with `res.json`, added `message` and `body` properties to various responses

--- controllers/testimonial-controller.js
+++ controllers/testimonial-controller.js
@@ -1,16 +1,25 @@
 const testimonialService = require("../services/testimonial-service");
 
-//http status
-const { OK, INTERNAL_SERVER_ERROR } = require("../constants/httpStatus");
+//http status codes
+const code = require("../constants/httpStatus");
+
+//http status messages 
+const message = require("../constants/message.js");
+
+//response messages
+const { GOT_TESTIMONIAL, CREATED_TESTIMONIAL, UPDATED_TESTIMONIAL, DELETED_TESTIMONIAL } = require("../constants/testimonial-constants.js")
 
 //GET all testimonials
 const list = async (req, res) => {
   try {
     const testimonials = await testimonialService.getTestimonials();
-    res.status(OK.code).send(testimonials);
+    res.status(code.OK).json({
+      message: GOT_TESTIMONIAL,
+      body: testimonials
+    });
   } catch (error) {
     console.error(error);
-    res.status(INTERNAL_SERVER_ERROR.code).send(INTERNAL_SERVER_ERROR.message);
+    res.status(code.INTERNAL_SERVER_ERROR).json(message.INTERNAL_SERVER_ERROR);
   }
 };
 
@@ -18,10 +27,13 @@ const list = async (req, res) => {
 const find = async (req, res) => {
   try {
     const testimonial = await testimonialService.getTestimonialById(req, res);
-    res.status(OK.code).send(testimonial);
+    res.status(code.OK).json({
+      message: GOT_TESTIMONIAL,
+      body: testimonial
+    });
   } catch (error) {
     console.error(error);
-    res.status(INTERNAL_SERVER_ERROR.code).send(INTERNAL_SERVER_ERROR.message);
+    res.status(code.INTERNAL_SERVER_ERROR).json(message.INTERNAL_SERVER_ERROR);
   }
 };
 
@@ -29,32 +41,42 @@ const find = async (req, res) => {
 const create = async (req, res) => {
   try {
     const testimonial = await testimonialService.createTestimonials(req, res);
-    res.status(OK.code).send(testimonial);
+    res.status(code.OK).json({
+      message: CREATED_TESTIMONIAL,
+      body: testimonial
+    });
   } catch (error) {
     console.error(error);
-    res.status(INTERNAL_SERVER_ERROR.code).send(INTERNAL_SERVER_ERROR.message);
+    res.status(code.INTERNAL_SERVER_ERROR).json(message.INTERNAL_SERVER_ERROR);
   }
 };
 
 //PATCH a testimonial by id
 const update = async (req, res) => {
+  let {id} = req.params;
   try {
     const testimonial = await testimonialService.updateTestimonial(req, res);
-    res.status(OK.code).send(testimonial);
+    res.status(code.OK).json({
+      message: UPDATED_TESTIMONIAL(id),
+      body: testimonial
+    });
   } catch (error) {
     console.error(error);
-    res.status(INTERNAL_SERVER_ERROR.code).send(INTERNAL_SERVER_ERROR.message);
+    res.status(code.INTERNAL_SERVER_ERROR).json(message.INTERNAL_SERVER_ERROR);
   }
 };
 
 //DELETE a testimonial by id
 const remove = async (req, res) => {
+  let {id} = req.params;
   try {
-    const testimonial = await testimonialService.deleteTestimonial(req, res);
-    res.status(OK.code).send(testimonial);
+    await testimonialService.deleteTestimonial(req, res);
+    res.status(code.OK).json({
+      message: DELETED_TESTIMONIAL(id)
+    });
   } catch (error) {
     console.error(error);
-    res.status(INTERNAL_SERVER_ERROR.code).send(INTERNAL_SERVER_ERROR.message);
+    res.status(code.INTERNAL_SERVER_ERROR).json(message.INTERNAL_SERVER_ERROR);
   }
 };
 
