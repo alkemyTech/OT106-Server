41dc1b786b4373c7e2a7a04d411a97474d326eb6
root <j.gaston.altamirano@gmail.com>
2021-12-05 21:01:35 -0300

    Update user service

--- services/user-service.js
+++ services/user-service.js
@@ -1,6 +1,6 @@
 const { UserRepository } = require('../repositories');
-const { BAD_REQUEST: BAD_REQUEST_CODE } = require('../constants/httpStatus');
-const { BAD_REQUEST: BAD_REQUEST_MESSAGE } = require('../constants/message');
+const { BAD_REQUEST: BAD_REQUEST_CODE, NOT_FOUND: NOT_FOUND_CODE } = require('../constants/httpStatus');
+const { BAD_REQUEST: BAD_REQUEST_MESSAGE, NOT_FOUND: NOT_FOUND_MESSAGE } = require('../constants/message');
 
 module.exports = {
   findAllUsers: UserRepository.findAllUsers,
@@ -22,7 +22,22 @@ module.exports = {
     throw err;
   },
 
-  updateUser: UserRepository.updateUser,
+  updateUser: async (id, attributes) => {
+    const user = await UserRepository.findUserByPk(id);
+
+    // If the user is not found, a 404 error is thrown
+    // CatchAsync function catch error and send it to handleError
+    if (!user) {
+      const error = new Error();
+      error.message = NOT_FOUND_MESSAGE;
+      error.status = NOT_FOUND_CODE;
+      throw error;
+    }
+
+    const updatedUser = await UserRepository.updateUser(id, attributes);
+
+    return updatedUser;
+  },
 
   destroyUser: UserRepository.destroyUser,
 };
