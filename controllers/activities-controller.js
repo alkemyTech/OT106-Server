3a739b9c8e0a07535c3ffd3ac077904ec54088b8
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2021-12-05 21:26:56 -0300

    add library http-status and fix statuscodes

--- controllers/activities-controller.js
+++ controllers/activities-controller.js
@@ -2,39 +2,40 @@ require('dotenv').config()
 const activityService = require('../services/activities-services')
 const catchAsync = require('../functions/catchAsync')
 const path = require('path')
-const {CREATED,OK} = require('../constants/httpStatus')
+const httpStatus = require('http-status');
+const {OK} = require('../constants/message')
 
 
 const createActivity = catchAsync(async (request,response)=> {
     const activity = await activityService.createActivity(request,response)
-    return response.status(CREATED).send(activity)
+    return response.status(httpStatus.CREATED).send(activity)
 
 })
 
 const getActivityimage = catchAsync(async (request,response)=> {
     const activity = await activityService.getActivityimage(request,response)
-    return response.status(OK).sendFile(path.join(__dirname,process.env.IMAGE_PATH,activity.image))
+    return response.status(httpStatus.OK).sendFile(path.join(__dirname,process.env.IMAGE_PATH,activity.image))
 })
 const getActivity = catchAsync(async (request,response)=> {
     const activity = await activityService.getActivity(request,response)
-    return response.status(OK).send(activity)
+    return response.status(httpStatus.OK).send(activity)
 })
 
 const getActivities = catchAsync(async (request,response)=> {
     const activities = await activityService.getActivities(request,response)
-    return response.status(OK).send(activities)
+    return response.status(httpStatus.OK).send(activities)
 
 })
 
 const deleteActivity = catchAsync(async (request,response)=> {
     const activity = await activityService.deleteActivity(request,response)
-    return response.status(OK).send()
+    return response.status(httpStatus.OK).send(OK)
 
 })
 
 const editActivity = catchAsync(async (request,response)=> {
     const activity = await activityService.editActivity(request,response)
-    return response.status(OK).send({activity})
+    return response.status(httpStatus.OK).send({activity})
 
 })
 
