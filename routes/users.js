2250484f3ec2a16a95cea53f16817fa8b234cb2c
root <j.gaston.altamirano@gmail.com>
2021-12-08 13:58:27 -0300

    route added

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 const usersController = require('../controllers/user-controller');
 
+const authentication = require('../middleware/authentication');
 const { validateUpdateUser } = require('../validations/update-user-validation');
 
 /* GET users listing. */
@@ -10,7 +11,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-// PATCH
-router.patch('/:id', validateUpdateUser, usersController.updateUser);
+router.route('/:id')
+  .patch(validateUpdateUser, usersController.updateUser)
+  .delete(authentication, usersController.destroyUser);
 
 module.exports = router;
