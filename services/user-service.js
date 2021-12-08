52873f427e8de9e8c316a9d660e671088aef4782
root <j.gaston.altamirano@gmail.com>
2021-12-08 07:31:06 -0300

    added token to response when user is created

--- services/user-service.js
+++ services/user-service.js
@@ -1,6 +1,7 @@
 const { UserRepository } = require('../repositories');
 const { BAD_REQUEST: BAD_REQUEST_CODE, NOT_FOUND: NOT_FOUND_CODE } = require('../constants/httpStatus');
 const { BAD_REQUEST: BAD_REQUEST_MESSAGE, NOT_FOUND: NOT_FOUND_MESSAGE } = require('../constants/message');
+const { generateAccesToken } = require('../functions/jsonwebtoken');
 
 module.exports = {
   findAllUsers: UserRepository.findAllUsers,
@@ -12,7 +13,12 @@ module.exports = {
   createUser: async (attributes) => {
     const result = await UserRepository.createUser(attributes);
 
-    if (result !== null) return result;
+    if (result !== null) {
+      // it add token when user is created
+      result.token = generateAccesToken(result);
+
+      return result;
+    }
 
     // The result as null means that the user can't be created
     // with that email (because the repository use findOrCreate)
