80482dbb82d79af1e75ec2f653b8bc1fba6afc95
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2021-12-07 21:55:49 -0300

    add sender email

---
+++ functions/mail-engine.js
@@ -0,0 +1,24 @@
+require('dotenv') 
+const sgMail = require('@sendgrid/mail')
+
+
+function SendEmail(email,subject,templateId){
+    sgMail.setApiKey(process.env.SENDGRID_APIKEY)
+const msg = {
+  to: email, // Change to your recipient
+  from:  process.env.VERIFIED_MAIL, // Change to your verified sender
+  subject: subject,
+  templateId: templateId,
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+}
+module.exports = SendEmail
+
+
