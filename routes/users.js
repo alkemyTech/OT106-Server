fd1acbb20f83d7b35c71c75676e314cbf3deb84f
Aguirre Gonzalo Adolfo <gonza.a.aguirre@gmail.com>
2021-12-09 17:53:01 -0300

    feat(endpoint): adds endpoint to list users

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 const usersController = require('../controllers/user-controller');
-
+const adminAuthentication = require('../middleware/admin-authentication');
 const { validateUpdateUser } = require('../validations/update-user-validation');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// List all the users
+router.get('/', adminAuthentication, usersController.findAllUsers);
 
 // PATCH
 router.patch('/:id', validateUpdateUser, usersController.updateUser);
