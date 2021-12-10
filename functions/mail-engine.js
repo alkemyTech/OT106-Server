b0af3c53b32845d83bc20f2d4f4b6f562cecbd5c
Santiago Agustín Moreno <santiago.moreno.2009@gmail.com>
2021-12-10 16:04:45 -0300

    refactor: removed unused `sendgrid` package, refactored mail-engine.js

--- functions/mail-engine.js
+++ functions/mail-engine.js
@@ -1,21 +1,25 @@
-require('dotenv') 
-const sgMail = require('@sendgrid/mail')
-function SendEmail(email,templateId){
-    sgMail.setApiKey(process.env.SENDGRID_APIKEY)
-const msg = {
-  to: email, // Change to your recipient
-  from: process.env.VERIFIED_MAIL, // Change to your verified sender
-  templateId: templateId,
-}
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent')
-  })
-  .catch((error) => {
-    console.error(error)
-  })
-}
-module.exports = SendEmail
+require("dotenv");
+const sgMail = require("@sendgrid/mail");
 
+const { VERIFIED_MAIL, SENDGRID_APIKEY } = process.env;
 
+function SendEmail(email, templateId) {
+  //Set up Sendgrid API KEY
+  sgMail.setApiKey(SENDGRID_APIKEY);
+
+  //Email message
+  const msg = {
+    to: email, // Change to your recipient
+    from: VERIFIED_MAIL, // Change to your verified sender
+    templateId,
+  };
+
+  //Send Email
+  try {
+    sgMail.send(msg);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = SendEmail;
