4486bc87ec40ef40035bfbed30112778caf5e535
Manuel Lamas <moncholamas@gmail.com>
2021-12-12 22:07:20 -0300

    added endpoint with details for slides

--- routes/slides-router.js
+++ routes/slides-router.js
@@ -7,8 +7,12 @@ const router = express.Router();
 
 // list imageUrl and order for all slides
 router.get('/',
-    // adminAuthentication, // try to validate admin user
+    adminAuthentication, // try to validate admin user
     SlidesController.findAllSlides);
 
+// list details for slide
+router.get('/:id',
+    adminAuthentication, // try to validate admin user
+    SlidesController.findSlideByPk);
 
 module.exports = router;
