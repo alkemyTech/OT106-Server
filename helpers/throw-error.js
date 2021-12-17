966b3e2ecda7e6b38ebb2649ab339c30ba4ffc51
Santiago Agustín Moreno <santiago.moreno.2009@gmail.com>
2021-12-16 22:07:08 -0300

    refactor: added different NODE_ENV cases to the throw-error.js helper function

--- helpers/throw-error.js
+++ helpers/throw-error.js
@@ -1,19 +1,44 @@
-const { INTERNAL_SERVER_ERROR: INTERNAL_SERVER_ERROR_CODE } = require('../constants/httpStatus');
-const { INTERNAL_SERVER_ERROR: INTERNAL_SERVER_ERROR_MESSAGE } = require('../constants/message');
+const {
+  INTERNAL_SERVER_ERROR: INTERNAL_SERVER_ERROR_CODE,
+} = require("../constants/httpStatus");
+const {
+  INTERNAL_SERVER_ERROR: INTERNAL_SERVER_ERROR_MESSAGE,
+} = require("../constants/message");
 
 module.exports = (status, message, body) => {
   const errorToThrow = new Error();
-
   errorToThrow.customError = { status, message, body };
 
   if (![status, message].every(Boolean)) {
-    // This is an alert to the developer under a development environment
-    if (
-      typeof process.env.NODE_ENV === 'undefined' ||
-      process.env.NODE_ENV === 'development'
-    ) {
-      console.log('An error was thrown with an incorrect format');
-      console.log('All errors MUST have a status and a message');
+    switch (process.env.NODE_ENV) {
+      // This is an alert to the developer under a development environment
+      case "development":
+        console.log("An error was thrown with an incorrect format");
+        console.log("All errors MUST have a status and a message");
+        console.log(
+          "Returning 500 status code and Internal Server Error Message"
+        );
+        break;
+
+      case "test":
+        //TODO
+        break;
+
+      case "production":
+        //TODO
+        break;
+
+      default:
+        console.log(
+          "Something went wrong when checking 'process.env.NODE_ENV' "
+        );
+        console.log(
+          "This variable should be set to 'test', 'development', or 'production'"
+        );
+        console.log(
+          "Returning 500 status code and Internal Server Error Message"
+        );
+        break;
     }
 
     // When the error has a wrong format, throw a default one
