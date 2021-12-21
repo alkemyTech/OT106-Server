217a419f2c122b2f8d4efa674e3656d9599835f8
root <j.gaston.altamirano@gmail.com>
2021-12-20 22:58:07 -0300

    GET /users

---
+++ test/get-all-users-test.js
@@ -0,0 +1,87 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const server = require('../app');
+const status = require('../constants/httpStatus');
+const { generateAccesToken } = require('../functions/jsonwebtoken');
+
+const assert = chai.assert;
+
+const PATH = '/users';
+
+const adminUserToken = generateAccesToken({ id: 1, email: 'test@test.com', roleId: 1 });
+const standardUserToken = generateAccesToken({ id: 1, email: 'test@test.com', roleId: 2 });
+
+chai.use(chaiHttp);
+
+describe('Get all users', () => {
+  describe('succesfully', () => {
+    it('when user is admin', done => {
+      chai
+        .request(server)
+        .get(PATH)
+        .set('Authorization', `Bearer ${adminUserToken}`)
+        .end((err, res) => {
+          assert.isNull(err);
+          assert.equal(res.status, status.OK);
+          assert.isArray(res.body);
+        });
+
+      done();
+    });
+  });
+
+  describe('fails', () => {
+    it('when user is not admin', done => {
+      chai
+        .request(server)
+        .get(PATH)
+        .set('Authorization', `Bearer ${standardUserToken}`)
+        .end((err, res) => {
+          assert.isNull(err);
+          assert.equal(res.status, status.FORBIDDEN);
+        });
+
+      done();
+    });
+
+    it('when request not contains token', done => {
+      chai
+        .request(server)
+        .get(PATH)
+        .end((err, res) => {
+          assert.isNull(err);
+          assert.equal(res.status, status.FORBIDDEN);
+        });
+
+      done();
+    });
+
+    it('when token is invalid', done => {
+      chai
+        .request(server)
+        .get(PATH)
+        .set('Authorization', 'Bearer invalidtoken')
+        .end((err, res) => {
+          assert.isNull(err);
+          assert.equal(res.status, status.FORBIDDEN);
+        });
+
+      done();
+    });
+
+    // TODO:
+    // it('when token has expired', done => {
+    //  chai
+    //    .request(server)
+    //    .get(PATH)
+    //    .set('Authorization', `Bearer ${expiredToken}`)
+    //    .end((err, res) => {
+    //      assert.isNull(err);
+    //      assert.equal(res.status, status.FORBIDDEN);
+    //    });
+    //
+    //  done();
+    // });
+  });
+});
