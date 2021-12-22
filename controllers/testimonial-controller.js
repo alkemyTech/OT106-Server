f28abbfbfe8142bd4061044561125e079c71f840
BuildTools <santiago.moreno.2009@gmail.com>
2021-12-22 19:23:43 -0300

    refactor: implemented catchAsync function in testimonials controller

--- controllers/testimonial-controller.js
+++ controllers/testimonial-controller.js
@@ -1,84 +1,65 @@
 const testimonialService = require("../services/testimonial-service");
+const catchAsync = require("../functions/catchAsync");
 
 //http status codes
 const code = require("../constants/httpStatus");
 
-//http status messages 
+//http status messages
 const message = require("../constants/message.js");
 
 //response messages
-const { GOT_TESTIMONIAL, CREATED_TESTIMONIAL, UPDATED_TESTIMONIAL, DELETED_TESTIMONIAL } = require("../constants/testimonial-constants.js")
+const {
+  GOT_TESTIMONIAL,
+  CREATED_TESTIMONIAL,
+  UPDATED_TESTIMONIAL,
+  DELETED_TESTIMONIAL,
+} = require("../constants/testimonial-constants.js");
 
 //GET all testimonials
-const list = async (req, res) => {
-  try {
-    const testimonials = await testimonialService.getTestimonials(req, res);
-    res.status(code.OK).json({
-      message: GOT_TESTIMONIAL,
-      body: testimonials
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(code.INTERNAL_SERVER_ERROR).json(message.INTERNAL_SERVER_ERROR);
-  }
-};
+const list = catchAsync(async (req, res, next) => {
+  const testimonials = await testimonialService.getTestimonials(req, res);
+  res.status(code.OK).json({
+    message: GOT_TESTIMONIAL,
+    body: testimonials,
+  });
+});
 
 //GET one testimonial by id
-const find = async (req, res) => {
-  try {
-    const testimonial = await testimonialService.getTestimonialById(req, res);
-    res.status(code.OK).json({
-      message: GOT_TESTIMONIAL,
-      body: testimonial
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(code.INTERNAL_SERVER_ERROR).json(message.INTERNAL_SERVER_ERROR);
-  }
-};
+const find = catchAsync(async (req, res, next) => {
+  const testimonial = await testimonialService.getTestimonialById(req, res);
+  res.status(code.OK).json({
+    message: GOT_TESTIMONIAL,
+    body: testimonial,
+  });
+});
 
 //POST a testimonial
-const create = async (req, res) => {
-  try {
-    const testimonial = await testimonialService.createTestimonial(req, res);
-    res.status(code.OK).json({
-      message: CREATED_TESTIMONIAL,
-      body: testimonial
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(code.INTERNAL_SERVER_ERROR).json(message.INTERNAL_SERVER_ERROR);
-  }
-};
+const create = catchAsync(async (req, res, next) => {
+  const testimonial = await testimonialService.createTestimonial(req, res);
+  res.status(code.OK).json({
+    message: CREATED_TESTIMONIAL,
+    body: testimonial,
+  });
+});
 
 //PATCH a testimonial by id
-const update = async (req, res) => {
-  let {id} = req.params;
-  try {
-    const testimonial = await testimonialService.updateTestimonial(req, res);
-    res.status(code.OK).json({
-      message: UPDATED_TESTIMONIAL(id),
-      body: testimonial
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(code.INTERNAL_SERVER_ERROR).json(message.INTERNAL_SERVER_ERROR);
-  }
-};
+const update = catchAsync(async (req, res, next) => {
+  let { id } = req.params;
+  const testimonial = await testimonialService.updateTestimonial(req, res);
+  res.status(code.OK).json({
+    message: UPDATED_TESTIMONIAL(id),
+    body: testimonial,
+  });
+});
 
 //DELETE a testimonial by id
-const remove = async (req, res) => {
-  let {id} = req.params;
-  try {
-    await testimonialService.deleteTestimonial(req, res);
-    res.status(code.OK).json({
-      message: DELETED_TESTIMONIAL(id)
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(code.INTERNAL_SERVER_ERROR).json(message.INTERNAL_SERVER_ERROR);
-  }
-};
+const remove = catchAsync(async (req, res, next) => {
+  let { id } = req.params;
+  await testimonialService.deleteTestimonial(req, res);
+  res.status(code.OK).json({
+    message: DELETED_TESTIMONIAL(id),
+  });
+});
 
 module.exports = {
   list,
