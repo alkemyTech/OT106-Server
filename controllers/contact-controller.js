9884939bdd02da90c5671547c41c74ee0c12dff7
BuildTools <santiago.moreno.2009@gmail.com>
2021-12-22 19:16:34 -0300

    refactor: implemented catchAsync in contacts controller

--- controllers/contact-controller.js
+++ controllers/contact-controller.js
@@ -1,10 +1,8 @@
 const contactService = require("../services/contact-service");
+const catchAsync = require("../functions/catchAsync");
 
 //http status codes
 const code = require("../constants/httpStatus");
-
-//http status messages
-const message = require("../constants/message.js");
 
 //response messages
 const {
@@ -15,75 +13,50 @@ const {
 } = require("../constants/contact-constants");
 
 //POST a contact
-const create = async (req, res) => {
-  try {
-    const contact = await contactService.createContact(req, res);
-    res.status(code.OK).json({
-      message: CREATED_CONTACT,
-      body: contact,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(code.INTERNAL_SERVER_ERROR).json(message.INTERNAL_SERVER_ERROR);
-  }
-};
+const create = catchAsync(async (req, res) => {
+  const contact = await contactService.createContact(req, res);
+  res.status(code.OK).json({
+    message: CREATED_CONTACT,
+    body: contact,
+  });
+});
 
 //Get all contacts
-const list = async (req, res) => {
-  try {
-    const contacts = await contactService.getContacts(req, res);
-    res.status(code.OK).json({
-      message: GOT_CONTACT,
-      body: contacts,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(code.INTERNAL_SERVER_ERROR).json(message.INTERNAL_SERVER_ERROR);
-  }
-};
+const list = catchAsync(async (req, res) => {
+  const contacts = await contactService.getContacts(req, res);
+  res.status(code.OK).json({
+    message: GOT_CONTACT,
+    body: contacts,
+  });
+});
 
 //Get contact by id
-const find = async (req, res) => {
-  try {
-    const contact = await contactService.getContactById(req, res);
-    res.status(code.OK).json({
-      message: GOT_CONTACT,
-      body: contact,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(code.INTERNAL_SERVER_ERROR).json(message.INTERNAL_SERVER_ERROR);
-  }
-};
+const find = catchAsync(async (req, res) => {
+  const contact = await contactService.getContactById(req, res);
+  res.status(code.OK).json({
+    message: GOT_CONTACT,
+    body: contact,
+  });
+});
 
 //PATCH a contact by id
-const update = async (req, res) => {
+const update = catchAsync(async (req, res) => {
   let { id } = req.params;
-  try {
-    const contact = await contactService.updateContact(req, res);
-    res.status(code.OK).json({
-      message: UPDATED_CONTACT(id),
-      body: contact,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(code.INTERNAL_SERVER_ERROR).json(message.INTERNAL_SERVER_ERROR);
-  }
-};
+  const contact = await contactService.updateContact(req, res);
+  res.status(code.OK).json({
+    message: UPDATED_CONTACT(id),
+    body: contact,
+  });
+});
 
 //DELETE contact by id
-const remove = async (req, res) => {
+const remove = catchAsync(async (req, res) => {
   let { id } = req.params;
-  try {
-    await contactService.deleteContact(req, res);
-    res.status(code.OK).json({
-      message: DELETED_CONTACT(id),
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(code.INTERNAL_SERVER_ERROR).json(message.INTERNAL_SERVER_ERROR);
-  }
-};
+  await contactService.deleteContact(req, res);
+  res.status(code.OK).json({
+    message: DELETED_CONTACT(id),
+  });
+});
 
 module.exports = {
   create,
