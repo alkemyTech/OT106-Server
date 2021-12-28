848452cf6dffc3d515dd8fcb9c7b6109773a67ce
Aguirre Gonzalo Adolfo <gonza.a.aguirre@gmail.com>
2021-12-27 21:37:40 -0300

    refactor: changes at swaggerDefinition

--- config/swagger-config.js
+++ config/swagger-config.js
@@ -8,28 +8,20 @@ const swaggerDefinition = {
       title: "SOMOS MAS API",
       description: "API para ONG Somos MAS",
       version: '1.0.0',
-      license: {
-        name: 'Licensed Under MIT',
-        url: '',
-      },
-      contact: {
-        name: 'contacto',
-        url: '',
-      },
     },
     components: {
-        securitySchemes: {
-          bearerAuth: {
+      securitySchemes: {
+        bearerAuth: {
           type: process.env.SWAGGER_SECURITY_TYPE,
-            scheme: process.env.SECURITY_SCHEMA,
-            bearerFormat: process.env.BEARER_FORMAT,
-          }
+          scheme: process.env.SECURITY_SCHEMA,
+          bearerFormat: process.env.BEARER_FORMAT,
         }
-      },
+      }
+    },
     servers: [
       {
         url: `${process.env.PROTOCOL}://${process.env.ENVIRONMENT}:${process.env.PORT}`,
-        description: 'Development server',
+        description: 'API server',
       },
     ]
   };
