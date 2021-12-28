54c4f6327403ede2f21e980203da5a2a02c9a43c
Aguirre Gonzalo Adolfo <gonza.a.aguirre@gmail.com>
2021-12-27 21:11:23 -0300

    fix: removes duplicate route

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const app = express();
 app.use(cors());
 
 //DOCUMENTATION//
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -64,7 +64,6 @@ app.use('/slides', slidesRouter);
 
 app.use('/categories', categoriesRouter);
 app.use('/contacts', contactRouter)
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
