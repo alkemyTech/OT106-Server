7c2e970bfd125d60a2d7a6d3aec759b37e45d489
SMoreno-dev <santiago.moreno.2009@gmail.com>
2021-12-29 18:54:35 -0300

    feat: added expired token tests for contacts and testimonials; added amazon s3 request data to testimonial POST endpoint tests

--- test/testimonial-delete.js
+++ test/testimonial-delete.js
@@ -7,8 +7,16 @@ const assert = chai.assert;
 chai.use(chaiHttp);
 
 //TOKEN
-const { generateAccessToken } = require("../functions/jsonwebtoken");
+const {
+  generateAccessToken,
+  generateAccessTokenExpired,
+} = require("../functions/jsonwebtoken");
 const TOKEN = generateAccessToken({ id: 1, email: "test@test.com", roleId: 1 });
+const EXPIRED_TOKEN = generateAccessTokenExpired({
+  id: 1,
+  email: "test@test.com",
+  roleId: 1,
+});
 
 //Constants files
 const code = require("../constants/httpStatus");
@@ -55,20 +63,19 @@ describe(`${ENDPOINT.METHOD} ${ENDPOINT.PATH}`, () => {
         });
     });
 
-    // TODO: Generate expired Token
-    // it("token (expired)", (done) => {
-    //   chai
-    //     .request(server)
-    //     .delete(`${ENDPOINT.PATH}/3`)
-    //     .set("Authorization", `Bearer ${EXPIRED_TOKEN}`)
-    //     .end((err, res) => {
-    //       assert.isNull(err);
-    //       assert.equal(res.status, code.FORBIDDEN);
-    //       assert.equal(res.body.message, message.FORBIDDEN);
+    it("token (expired)", (done) => {
+      chai
+        .request(server)
+        .delete(`${ENDPOINT.PATH}/3`)
+        .set("Authorization", `Bearer ${EXPIRED_TOKEN}`)
+        .end((err, res) => {
+          assert.isNull(err);
+          assert.equal(res.status, code.FORBIDDEN);
+          assert.equal(res.body.message, message.FORBIDDEN);
 
-    //       done();
-    //     });
-    // });
+          done();
+        });
+    });
 
     it("token (invalid)", (done) => {
       chai
