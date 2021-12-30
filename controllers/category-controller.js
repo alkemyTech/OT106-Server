c1c5be207ca5fc4e0dacc9416374a6dfbb866ec8
Cristianlell <cristianlell24@gmail.com>
2021-12-29 23:40:07 -0300

    agrego S3 a category y manejador de errores

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,93 +1,57 @@
 const { list, detail, create, update, remove } = require('../services/category-service');
 const httpStatus = require('../constants/httpStatus');
 const message = require('../constants/message')
-
+const catchAsync = require('../functions/catchAsync')
 
 module.exports = {
-  list: async (req, res) => {
+  list: catchAsync(async (req, res) => {
     let page = +req.params.page;
 
-    try {
-      let categories = await list(req,page);
-      return res.status(httpStatus.OK).json({ message: message.OK, body: categories });
-      
-    } catch (error) {
-      return res.status(httpStatus.INTERNAL_SERVER_ERROR || error.status).json({ message:message.INTERNAL_SERVER_ERROR || error });
+    let categories = await list(req, page);
+    return res.status(httpStatus.OK).json({ message: message.OK, body: categories });
+  }),
 
-    }
-  },
+  detail: catchAsync(async (req, res) => {
+    let id = req.params.id;
 
-  detail: async (req, res) => {
-    let id  = req.params.id;
+    let category = await detail(id);
 
-    try {
+    return res.status(httpStatus.OK).json({ message: message.OK, body: category });
+  }),
 
-      let category = await detail(id);
-      if (category) {
-
-        return res.status(httpStatus.OK).json({ message: message.OK, body: category });
-        
-      } else {
-        return res.status(httpStatus.NOT_FOUND).json({ message: message.NO_CONTENT });
-        
-      }
-
-    } catch (error) {
-      console.log(error);
-      return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: message.INTERNAL_SERVER_ERROR });
-
-    }
-  },
-  create: async (req, res) => {
+  create: catchAsync(async (req, res) => {
 
     req.body.image = req.file
 
-    try {
-      let category = await create(req.body);
-      console.log( await category);
-      return res.status(httpStatus.CREATED).json({ message: message.CREATED,body:category.dataValues});
-        
-    } catch (error) {
-      console.log(error);
-      return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: message.INTERNAL_SERVER_ERROR });
+    let category = await create(req.body);
+    console.log(await category);
+    return res.status(httpStatus.CREATED).json({ message: message.CREATED, body: category.dataValues });
 
-    }
-  },
-  update: async (req, res) => {
+  }),
+  
+  update: catchAsync(async (req, res) => {
     req.body.image = req.file
-    try {
-      let response = await update(req.params.id, req.body);
-      console.log(response);
-      if (response[0] === 0) {
-        return res.status(httpStatus.BAD_REQUEST).json({message : message.BAD_REQUEST});
 
-      }
-      
-      return res.status(httpStatus.CREATED).json({message: message.CREATED,body:response})
-        
-    } catch (error) {
+    let response = await update(req.params.id, req.body);
+    console.log(response);
+    if (response[0] === 0) {
+      return res.status(httpStatus.BAD_REQUEST).json({ message: message.BAD_REQUEST });
+    }
 
-      return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({message:message.INTERNAL_SERVER_ERROR});
+    return res.status(httpStatus.CREATED).json({ message: message.CREATED, body: response })
+
+  }),
+
+  remove: catchAsync( async (req, res) => {
+
+    let response = await remove(req.params.id);
+
+    if (response === 0) {
+      return res.status(httpStatus.NOT_FOUND).json({ message: message.NOT_FOUND, body: response });
 
     }
-  },
-  remove: async (req, res) => {
 
-    try {
-      let response = await remove(req.params.id);
-      console.log(response);
-      if (response === 0) {
-        return res.status(httpStatus.NOT_FOUND).json({message:message.NOT_FOUND,body:response});
-
-      }
-      
-      return res.status(httpStatus.OK).json({message:message.OK,body:response})
-        
-    } catch (error) {
-
-      return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({message:message.INTERNAL_SERVER_ERROR});
-
-    }
-  },
+    return res.status(httpStatus.OK).json({ message: message.OK, body: response })
+  }),
 
 }
--- services/category-service.js
+++ services/category-service.js
@@ -1,40 +1,79 @@
 const categoriesRepository = require('../repositories/category-repository');
 const paginate = require('../services/paginate');
 const limit = 10;
+const { uploadFileToAmazonS3Bucket: uploadFile } = require('./amazon-s3-service')
+const throwError = require("../functions/throw-error");
+const code = require("../constants/httpStatus");
+const message = require("../constants/message");
+
 module.exports = {
-  list: async (req) =>{
+  list: async (req) => {
     const count = await categoriesRepository.count()
     console.log(count);
-    const resultPagination = paginate.pagination(limit,count,req,'categories')
+    const resultPagination = paginate.pagination(limit, count, req, 'categories')
 
 
-    const categories = await categoriesRepository.list(limit,resultPagination.offset)
+    const categories = await categoriesRepository.list(limit, resultPagination.offset)
     let response = {
       max: resultPagination.max,
       previousPage: resultPagination.previousPageUrl,
       nextPage: resultPagination.nextPageUrl,
-      data:categories
+      data: categories
     }
 
     if (page == 1) response.previousPage = null;
     if (page == resultPagination.lastPage) response.nextPage = null;
     /////////////////////////////////////////////////////////////////////
+    if (!categories[0]) {
+      return throwError(code.NOT_FOUND, message.NOT_FOUND);
+    }
     return response;
 
 
   },
-  detail: id => categoriesRepository.detail(id),
-  create:async (body) => {
+
+  detail: async (id) => {
+    let category = await categoriesRepository.detail(id);
+
+    if (!category) {
+      return throwError(code.NOT_FOUND, message.NOT_FOUND);
+    }
+    return category
+  },
+
+  create: (async (body) => {
     if (body.image) {
-      body.image = await uploadFile(body.image);
+      let fileName = body.image.originalname.split('.')[0];
+      let buffer = body.image.buffer;
+      body.image = await uploadFile(fileName, buffer);
     }
     return categoriesRepository.createCategory(body)
-  },
+  }),
+  
   update: async (id, body) => {
     if (body.image) {
-      body.image = await uploadFile(body.image);
+  
+      let buffer = body.image.buffer;
+      let fileName = body.image.originalname.split('.')[0];
+      body.image = await uploadFile(fileName, buffer);
     }
-    return categoriesRepository.updateCategory(id, body);
+    let category = await categoriesRepository.detail(id);
+
+    if (!category) {
+      return throwError(code.NOT_FOUND, message.NOT_FOUND);
+    }
+    let update = categoriesRepository.updateCategory(id, body);
+
+    return update
   },
-  remove: id => categoriesRepository.deleteCategory(id)
+
+  remove: async (id) => {
+    let category = await categoriesRepository.detail(id);
+
+    if (!category) {
+      return throwError(code.NOT_FOUND, message.NOT_FOUND);
+    }
+    let remove = await categoriesRepository.deleteCategory(id)
+    return remove;
+  }
 };
