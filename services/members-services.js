421e3a45d98933f18d460d569d26bd393208688d
Manuel Lamas <moncholamas@gmail.com>
2022-01-02 20:46:17 -0300

    fixed members flow

--- services/members-services.js
+++ services/members-services.js
@@ -1,48 +1,49 @@
 const membersRepository = require('../repositories/members-repository');
 const paginate = require('../services/paginate');
 const { MEMBER_EXISTS } = require('../constants/member-constants');
-const throwError = require("../functions/throw-error");
+const throwError = require('../functions/throw-error');
 
 const limit = 10;
 
 module.exports = {
-    createMember: body => membersRepository.createMember(body),
+  createMember: body => membersRepository.createMember(body),
 
-    getMembersAll: async (req) => {
-        const max = await membersRepository.getMembersCount();
-        console.log(max);
-        const paginationData = paginate.pagination(limit, max, req, 'members');
+  getMembersAll: async (req) => {
+    const max = await membersRepository.getMembersCount();
+    const paginationData = paginate.pagination(limit, max, req, 'members');
 
-        const members = await membersRepository.getMembersAll(limit, paginationData.offset);
+    const members = await membersRepository.getMembersAll(limit, paginationData.offset);
 
-        let response = {
-            max: paginationData.max,
-            previousPage: paginationData.previousPageUrl,
-            nextPage: paginationData.nextPageUrl,
-            data: members
-        };
-        ///////////////////////DEFINE THE PAGES//////////////////////////////
-        if (page == 1) response.previousPage = null;
-        if (page == paginationData.lastPage) response.nextPage = null;
-        /////////////////////////////////////////////////////////////////////
-        return response;
-    },
+    const response = {
+      max: paginationData.max,
+      previousPage: paginationData.previousPageUrl,
+      nextPage: paginationData.nextPageUrl,
+      data: members
+    };
+        // /////////////////////DEFINE THE PAGES//////////////////////////////
+    if (page == 1) response.previousPage = null;
+    if (page == paginationData.lastPage) response.nextPage = null;
+        // ///////////////////////////////////////////////////////////////////
+    return response;
+  },
 
-    getMemberById: id => membersRepository.getMemberById(id),
+  getMemberById: id => membersRepository.getMemberById(id),
 
-    updateMember: async (id, body) => {
-        const memberExists = await memberService.getMemberById(req.params.id);
-        if (!memberExists) {
-            return throwError(code.NOT_FOUND, MEMBER_EXISTS);
-        }
-
-        membersRepository.updateMember(id, body);
-    },
-    deleteMember: async (id) => {
-        const memberExists = await memberService.getMemberById(req.params.id);
-        if (!memberExists) {
-            return throwError(code.NOT_FOUND, MEMBER_EXISTS);
-        }
-        membersRepository.deleteMember(id);
+  updateMember: async (id, body) => {
+    const memberExists = await membersRepository.getMemberById(id);
+    if (!memberExists) {
+      return throwError(code.NOT_FOUND, MEMBER_EXISTS);
     }
+
+    const result = await membersRepository.updateMember(id, body);
+    return result;
+  },
+  deleteMember: async (id) => {
+    const memberExists = await membersRepository.getMemberById(id);
+    if (!memberExists) {
+      return throwError(code.NOT_FOUND, MEMBER_EXISTS);
+    }
+    const result = await membersRepository.deleteMember(id);
+    return result;
+  }
 };
