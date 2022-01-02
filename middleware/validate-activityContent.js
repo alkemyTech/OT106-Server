4d92b6d97473d5de5828bb77b0c3196a6a65a3b4
Manuel Lamas <moncholamas@gmail.com>
2022-01-02 20:39:58 -0300

    fixed

--- middleware/validate-activityContent.js
+++ middleware/validate-activityContent.js
@@ -1,14 +1,15 @@
-const httpStatus = require('http-status')
-const {NOT_ACCEPTABLE} = require('../constants/message')
-const checkActivity = (request,response,next) => {
-    if (!request.body.name || !request.body.content || !request.file){
-        throwError(httpStatus.NOT_FOUND, NOT_FOUND)
-        return throwError(httpStatus.NOT_ACCEPTABLE, NOT_ACCEPTABLE)
-    }
-    next()
-}
+const httpStatus = require('../constants/httpStatus');
+const { NOT_ACCEPTABLE } = require('../constants/message');
+const throwError = require('../functions/throw-error');
+
+const checkActivity = (request, response, next) => {
+  if (!request.body.name || !request.body.content || !request.file) {
+    return throwError(httpStatus.NOT_ACCEPTABLE, NOT_ACCEPTABLE);
+  }
+  next();
+};
 
 
 module.exports = {
-    checkActivity
-}
+  checkActivity
+};
